1b6b657fd06609e7024698d0a9d785e14b8fee46
jkwong <jkwong@ca.ibm.com>
2019-08-29 13:36:50 +0000

    remove keys from provider configuration

--- provider.tf
+++ provider.tf
@@ -1,5 +1,3 @@
 provider "ibm" {
-  softlayer_username = "${var.ibm_sl_username}"
-  softlayer_api_key  = "${var.ibm_sl_api_key}"
   softlayer_timeout  = "300"
 }
--- variables.tf
+++ variables.tf
@@ -1,6 +1,3 @@
-variable "ibm_sl_username" {}
-variable "ibm_sl_api_key" {}
-
 variable "datacenter" {
   default = "wdc04"
 }
